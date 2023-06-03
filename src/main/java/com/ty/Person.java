974f1e88cb590f753875e53be3e478c11622f998
Thanujadr <thanujathanut@gmail.com>
2023-06-03 12:01:52 +0530

    first

---
+++ src/main/java/com/ty/Person.java
@@ -0,0 +1,55 @@
+package com.ty;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Person 
+{
+	@Id
+	private int id;
+	private String name;
+	private String address;
+	private long phno;
+	@OneToOne
+	private Pancard pancard;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public long getPhno() {
+		return phno;
+	}
+	public void setPhno(long phno) {
+		this.phno = phno;
+	}
+	public Pancard getPancard() {
+		return pancard;
+	}
+	public void setPancard(Pancard pancard) {
+		this.pancard = pancard;
+	}
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", address=" + address + ", phno=" + phno + ", pancard="
+				+ pancard + "]";
+	}
+	
+	
+
+}
